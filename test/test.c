19cb818cbc1f7616a39d38411ed46496d5e61658
Inkl1ng <scottbot2018@gmail.com>
2024-10-11 15:15:08 -0700

    Update tests to read integer values

--- test/test.c
+++ test/test.c
@@ -4,6 +4,12 @@ int main(int argc, char** argv)
 {
     inkINI_file ini_file = inkINI_load_file("test/ini-files/a.ini");
 
+    putchar('\n');
+    printf("key: %s\tvalue: %i\n", "age", inkINI_read_i(ini_file, "age"));
+    printf("key: %s\tvalue: %i\n", "income", inkINI_read_i(ini_file, "income"));
+    printf("key: %s\tvalue: %i\n", "num_cars", inkINI_read_i(ini_file, "num_cars"));
+    printf("key: %s\tvalue: %i\n", "debt", inkINI_read_i(ini_file, "debt"));
+
     inkINI_close_file(ini_file);
 
     return 0;
